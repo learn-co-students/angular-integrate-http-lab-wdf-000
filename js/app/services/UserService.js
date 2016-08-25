3ee5a57cbd9b9791eae835162af6a12d5edc9760
blaze.i.burgess@gmail.com <blaze.i.burgess@gmail.com>
2016-08-25 09:04:44 -0400

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,11 @@
 function UserService($http) {
+  this.getUser = function () {
+    return $http.get('http://0.0.0.0:8882/rest/user')
+  }
 
+  this.updateUser = function() {
+    return $http.post('http://0.0.0.0:8882/rest/user', {first_name: 'First', last_name: 'Last', email: 'qwe@asd.rty'});
+  }
 }
 
 angular
