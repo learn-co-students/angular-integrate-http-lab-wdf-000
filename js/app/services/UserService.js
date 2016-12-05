c23e051ded688c58be30e35dcf7a0f419d9b259a
Margaret <eutopia@gmail.com>
2016-12-05 17:45:37 -0500

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,10 @@
 function UserService($http) {
-
+	this.getUser = function() {
+		return $http.get('http://0.0.0.0:8882/rest/user');
+	};
+	this.updateUser = function(stuff) {
+		return $http.post('http://0.0.0.0:8882/rest/user', {data: "stuff"});
+	};
 }
 
 angular
